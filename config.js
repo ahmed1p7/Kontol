8f65c895cb590eb2b0c8ca45b6b8a17dddd41ba8
ahmed1p7 <82735720+ahmed1p7@users.noreply.github.com>
2022-07-19 04:13:58 +0200

    Update config.js

--- config.js
+++ config.js
@@ -83,10 +83,10 @@ global.hbunny = ['https://telegra.ph/file/2b71a8d46d29351479fbc.jpg', 'https://t
 global.hbeach = ['https://telegra.ph/file/14ae0ba2da77d74e6b80c.jpg', 'https://telegra.ph/file/b6905b77e6c7732592a13.jpg','https://telegra.ph/file/9da45a352eb4c40e5d641.jpg', 'https://telegra.ph/file/59e78846ee365975ee6e3.jpg','https://telegra.ph/file/1bf7dee46b83eb4c41d7d.jpg','https://telegra.ph/file/0525a7929f819cb8278f3.jpg']
 global.bg = ['https://telegra.ph/file/2b71a8d46d29351479fbc.jpg', 'https://telegra.ph/file/ae610571b62b5ab876e9c.jpg', 'https://telegra.ph/file/cc8255d5b989eef587af2.jpg','https://telegra.ph/file/30d2e7375996abd9cfee3.jpg']
 /*============== NOMOR ==============*/
-global.nomorbot = '6287818916348'
-global.nomorown = '6288210072756'
-global.namebot = 'LuccyBot'
-global.nameown = 'LuxxyXyz'
+global.nomorbot = ' 393271166550 '
+global.nomorown = ' 393271166550 '
+global.namebot = 'Mayo'
+global.nameown = 'Mayo'
 
 /*=========== FAKE SIZE ===========*/
 global.fsizedoc = '99999999999999' // default 10TB
